67a6328dedc2215073edc249ea2a3f6c473dc625
RavinduSathsara <rsathsara330@gmail.com>
2022-01-01 20:46:04 +0530

    Set async funtion

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import FoodCard from "../components/FoodCard";
 
 const Home = () => {
+  const AppId = "cf4f9486";
+  const AppKey = "c4878490c89ff0e78b40116db340994b";
+
+  // const exaampleReq = `https://api.edamam.com/search?q=chicken&app_id=${AppId}&app_key=${AppKey}`;
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${AppId}&app_key=${AppKey}`
+    );
+    const data = await response.json();
+    console.log(data);
+  };
   return (
     <div className="container">
-      <input
-        className="form-control m-5"
-        placeholder="Search Food"
-        type="text"
-      />
-      <div className="row">
-        <div className="col-md-4">
-          <FoodCard /> <FoodCard />
-          <FoodCard />
-        </div>
-      </div>
+      <form className="m-4">
+        <input className="form-control" type="text" />
+        <button className="btn btn-success" type="submit">
+          Search
+        </button>
+      </form>
     </div>
   );
 };
